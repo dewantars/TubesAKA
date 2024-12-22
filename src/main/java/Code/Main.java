3480d3d5042065ee24587cec87aed8440a3a5438
Dewanta Rahma Satria <dewantarahmasatria@gmail.com>
2024-12-22 09:51:13 +0700

    update connection GUI

--- src/main/java/Code/Main.java
+++ src/main/java/Code/Main.java
@@ -5,28 +5,16 @@ package Code;
 
 import java.util.Scanner;
 import java.math.BigInteger;
+import GUI.GUI;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        try {
-            System.out.println("Masukkan jumlah orang: ");
-            int orang = scan.nextInt();
-            System.out.println("Masukkan jumlah barang yang diperebutkan");
-            int barang = scan.nextInt();
-            if (orang <= 0 || barang <= 0) {
-                throw new IllegalArgumentException("Bilangan harus lebih besar dari 0.");
-            }
-            System.out.println("Hasil iteratif: " + PermutasiIteratif.permutasi(orang, barang));
-            System.out.println("Hasil Rekursif: " + PermutasiRekursif.permutasi(orang, barang));
-        } catch (IllegalArgumentException e) {
-            System.out.println("Kesalahan: " + e.getMessage());
-        } catch (Exception e) {
-            System.out.println("Input tidak valid. Harap masukkan bilangan integer positif.");
-        } finally {
-            scan.close();
-        }
+        GUI g = new GUI();
+        g.setVisible(true);
+        g.pack();
+        g.setLocationRelativeTo(null);
+        g.setDefaultCloseOperation(GUI.EXIT_ON_CLOSE);
     }
 }
--- src/main/java/GUI/GUI.java
+++ src/main/java/GUI/GUI.java
@@ -3,7 +3,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package GUI;
-
+import Code.PermutasiIteratif;
+import Code.PermutasiRekursif;
+import java.math.BigInteger;
 /**
  *
  * @author Dewanta Rahma Satria
@@ -28,17 +30,17 @@ public class GUI extends javax.swing.JFrame {
 
         jPanel3 = new javax.swing.JPanel();
         jLabel4 = new javax.swing.JLabel();
-        jPanel2 = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
+        jPanel_topbar = new javax.swing.JPanel();
+        jLabel_Close = new javax.swing.JLabel();
+        jLabel_MInimize = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jTextField2 = new javax.swing.JTextField();
+        jTextField_Unsur = new javax.swing.JTextField();
         jLabel5 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        jTextField_Elemen = new javax.swing.JTextField();
+        jButton_Iteratif = new javax.swing.JButton();
+        jButton_Rekursif = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextPane1 = new javax.swing.JTextPane();
+        jTextOutput = new javax.swing.JTextPane();
         jLabel6 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -50,43 +52,53 @@ public class GUI extends javax.swing.JFrame {
         jLabel4.setForeground(new java.awt.Color(0, 0, 0));
         jLabel4.setText("Aplikasi Perhitungan Permutasi");
 
-        jPanel2.setBackground(new java.awt.Color(200, 200, 200));
+        jPanel_topbar.setBackground(new java.awt.Color(200, 200, 200));
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
-        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel1.setText("X");
+        jLabel_Close.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jLabel_Close.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel_Close.setText("X");
+        jLabel_Close.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel_CloseMouseClicked(evt);
+            }
+        });
 
-        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(0, 0, 0));
-        jLabel3.setText("-");
+        jLabel_MInimize.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        jLabel_MInimize.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel_MInimize.setText("-");
+        jLabel_MInimize.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jLabel_MInimizeMouseClicked(evt);
+            }
+        });
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
+        javax.swing.GroupLayout jPanel_topbarLayout = new javax.swing.GroupLayout(jPanel_topbar);
+        jPanel_topbar.setLayout(jPanel_topbarLayout);
+        jPanel_topbarLayout.setHorizontalGroup(
+            jPanel_topbarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel_topbarLayout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jLabel_MInimize, javax.swing.GroupLayout.PREFERRED_SIZE, 15, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jLabel_Close, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        jPanel_topbarLayout.setVerticalGroup(
+            jPanel_topbarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel_topbarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addComponent(jLabel_MInimize, javax.swing.GroupLayout.PREFERRED_SIZE, 16, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jLabel_Close, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         jLabel2.setFont(new java.awt.Font("Gilroy-SemiBold", 0, 12)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(0, 0, 0));
         jLabel2.setText("Masukkan Jumlah Unsur");
 
-        jTextField2.setBackground(new java.awt.Color(221, 221, 221));
-        jTextField2.setFont(new java.awt.Font("Gilroy-SemiBold", 0, 12)); // NOI18N
-        jTextField2.addActionListener(new java.awt.event.ActionListener() {
+        jTextField_Unsur.setBackground(new java.awt.Color(221, 221, 221));
+        jTextField_Unsur.setFont(new java.awt.Font("Gilroy-SemiBold", 0, 12)); // NOI18N
+        jTextField_Unsur.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField2ActionPerformed(evt);
+                jTextField_UnsurActionPerformed(evt);
             }
         });
 
@@ -94,15 +106,30 @@ public class GUI extends javax.swing.JFrame {
         jLabel5.setForeground(new java.awt.Color(30, 30, 30));
         jLabel5.setText("Masukkan Jumlah Elemen");
 
-        jTextField1.setBackground(new java.awt.Color(221, 221, 221));
-        jTextField1.setText("jTextField1");
+        jTextField_Elemen.setBackground(new java.awt.Color(221, 221, 221));
+        jTextField_Elemen.setText("jTextField1");
 
-        jButton1.setText("Iteratif");
+        jButton_Iteratif.setText("Iteratif");
+        jButton_Iteratif.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_IteratifActionPerformed(evt);
+            }
+        });
 
-        jButton2.setText("Rekursif");
+        jButton_Rekursif.setText("Rekursif");
+        jButton_Rekursif.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton_RekursifMouseClicked(evt);
+            }
+        });
+        jButton_Rekursif.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_RekursifActionPerformed(evt);
+            }
+        });
 
-        jTextPane1.setBackground(new java.awt.Color(221, 221, 221));
-        jScrollPane1.setViewportView(jTextPane1);
+        jTextOutput.setBackground(new java.awt.Color(221, 221, 221));
+        jScrollPane1.setViewportView(jTextOutput);
 
         jLabel6.setFont(new java.awt.Font("Gilroy-SemiBold", 0, 12)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(30, 30, 30));
@@ -112,22 +139,22 @@ public class GUI extends javax.swing.JFrame {
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel_topbar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addGap(18, 18, 18)
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel3Layout.createSequentialGroup()
                         .addGap(59, 59, 59)
-                        .addComponent(jButton1)
+                        .addComponent(jButton_Iteratif)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton2)
+                        .addComponent(jButton_Rekursif)
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                     .addGroup(jPanel3Layout.createSequentialGroup()
                         .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                             .addComponent(jLabel2)
-                            .addComponent(jTextField2)
+                            .addComponent(jTextField_Unsur)
                             .addComponent(jLabel5)
-                            .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 395, Short.MAX_VALUE))
+                            .addComponent(jTextField_Elemen, javax.swing.GroupLayout.DEFAULT_SIZE, 395, Short.MAX_VALUE))
                         .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -144,21 +171,21 @@ public class GUI extends javax.swing.JFrame {
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPanel_topbar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jLabel4)
                 .addGap(36, 36, 36)
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jTextField_Unsur, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jLabel5)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jTextField_Elemen, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(29, 29, 29)
                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                    .addComponent(jButton_Iteratif)
+                    .addComponent(jButton_Rekursif))
                 .addGap(30, 30, 30)
                 .addComponent(jLabel6)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -172,9 +199,38 @@ public class GUI extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField2ActionPerformed
+    private void jTextField_UnsurActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField_UnsurActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField2ActionPerformed
+    }//GEN-LAST:event_jTextField_UnsurActionPerformed
+
+    private void jLabel_CloseMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel_CloseMouseClicked
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_jLabel_CloseMouseClicked
+
+    private void jLabel_MInimizeMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jLabel_MInimizeMouseClicked
+        // TODO add your handling code here:
+        this.setState(1);
+    }//GEN-LAST:event_jLabel_MInimizeMouseClicked
+
+    private void jButton_IteratifActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_IteratifActionPerformed
+        // TODO add your handling code here:
+        BigInteger result = PermutasiIteratif.permutasi(Integer.parseInt(jTextField_Unsur.getText()), Integer.parseInt(jTextField_Elemen.getText()));
+        String value = result.toString();
+        jTextOutput.setText(value);
+    }//GEN-LAST:event_jButton_IteratifActionPerformed
+
+    private void jButton_RekursifMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton_RekursifMouseClicked
+        
+    }//GEN-LAST:event_jButton_RekursifMouseClicked
+
+    private void jButton_RekursifActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_RekursifActionPerformed
+        // TODO add your handling code here:
+        // TODO add your handling code here:
+        BigInteger result = PermutasiRekursif.permutasi(Integer.parseInt(jTextField_Unsur.getText()), Integer.parseInt(jTextField_Elemen.getText()));
+        String value = result.toString();
+        jTextOutput.setText(value);
+    }//GEN-LAST:event_jButton_RekursifActionPerformed
 
     /**
      * @param args the command line arguments
@@ -212,19 +268,19 @@ public class GUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JButton jButton_Iteratif;
+    private javax.swing.JButton jButton_Rekursif;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel jLabel_Close;
+    private javax.swing.JLabel jLabel_MInimize;
     private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel_topbar;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextPane jTextPane1;
+    private javax.swing.JTextField jTextField_Elemen;
+    private javax.swing.JTextField jTextField_Unsur;
+    private javax.swing.JTextPane jTextOutput;
     // End of variables declaration//GEN-END:variables
 }
